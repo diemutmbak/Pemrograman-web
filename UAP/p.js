8b12b232a081231b33b2ba00369d23a9f27edebb
diemutmbak <127311730+diemutmbak@users.noreply.github.com>
2023-12-29 14:05:21 +0700

    Add files via upload

---
+++ UAP/p.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+const kodeInput = document.getElementById('kode');
+const jumlahInput = document.getElementById('jumlah');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+
+
+let barang = [
+    { kode: "001", nama: "Barang A", harga: 10000 },
+    { kode: "002", nama: "Barang B", harga: 20000},
+    { kode: "003", nama: "Barang C", harga: 30000},
+]
+}
